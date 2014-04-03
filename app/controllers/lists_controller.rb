4e0b147f12a0d48facc8bd402d23d5fb57175fda
Nick Peterson <nick@hindiemedia.com>
2014-04-02 20:36:46 -0600

    third commit with css updates

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,6 +17,32 @@ class ListsController < ApplicationController
 	    #redirect_to root_path
     end
 
+    def edit
+	    @list = List.find( params[:id])
+	end
+
+	def show
+    	@list = List.find params[:id]
+    	 redirect_to root_path
+  	end
+
+	def update
+	   @list = List.find( params[:id])
+	   safe_list_params = params.require(:list).permit(:name, :date_created)
+
+	   if @list.update(safe_list_params)
+	      redirect_to @list
+	   else
+	      render 'edit'
+	   end
+	end
+
+    def destroy
+	    list = List.find(params[:id])
+	    list.destroy
+	    redirect_to root_path
+	  end
+
 private
 	     def list_params
 		    params.require(:list).permit(:name, :date_created)
