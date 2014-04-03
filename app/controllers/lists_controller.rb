b431d17bd778878c1aa8aed3788cdd5c3f06f1ab
Nick Peterson <nick@hindiemedia.com>
2014-04-02 19:15:28 -0600

    first commit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,27 +4,22 @@ class ListsController < ApplicationController
 	end
 
 	def create
-    @list = List.new(params[:list])
+    @list = List.new(list_params)
 	    if @list.save
 	      redirect_to root_path
 	  	else
 	  	  render :new
 	  	end
-
-	  	private
-	     def list_params
-		    params.require(:list).permit(:name, :date_created)
-		 end
     end
 
     def new
 	    @list = List.new
-	    respond_to do |format|
-	    	format.html
-	    	format.json {render json: @list}
-	    end
 	    #redirect_to root_path
     end
 
+private
+	     def list_params
+		    params.require(:list).permit(:name, :date_created)
+		 end
     
 end
